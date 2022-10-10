2532ef3b2d646afe9c9601c47260ea00b70cec43
Oliwia Gowor <72342415+OliwiaGowor@users.noreply.github.com>
2022-10-10 20:31:54 +0200

    added copying to clipboard, fized overflow on mobile devices

--- script.js
+++ script.js
@@ -1,48 +1,59 @@
 //hamburger icon opening
-document.querySelector('.hamburger-icon-mobile').addEventListener('click', function() {
-	this.classList.toggle('open');
-	document.querySelector('.header-nav').classList.toggle('open');
-	document.querySelector('.header-nav').classList.toggle('mobile');
-});
-
-var navElements = document.querySelectorAll('.nav-element');
-for (i = 0; i < navElements.length; i++) {
-	navElements[i].addEventListener('click', function() {
-	document.querySelector('.hamburger-icon-mobile').classList.toggle('open');
-	document.querySelector('.header-nav').classList.remove('open');
-	document.querySelector('.header-nav').classList.remove('mobile');
+function hamburgerIconHandler() {
+	document.querySelector('.hamburger-icon-mobile').addEventListener('click', function() {
+		this.classList.toggle('open');
+		document.querySelector('.header-nav').classList.toggle('open');
+		document.querySelector('.header-nav').classList.toggle('mobile');
 	});
 }
+hamburgerIconHandler();
+
+
+//mobile dropdown opening
+function mobileDropdownHandler() {
+	var navElements = document.querySelectorAll('.nav-element');
+	for (i = 0; i < navElements.length; i++) {
+		navElements[i].addEventListener('click', function() {
+		document.querySelector('.hamburger-icon-mobile').classList.toggle('open');
+		document.querySelector('.header-nav').classList.remove('open');
+		document.querySelector('.header-nav').classList.remove('mobile');
+		});
+	}
+}
+mobileDropdownHandler();
 
 
 //website color changing
-const colorSelect = document.querySelector('.color-circle');
-const colorStylessheetLink = document.querySelector('.colorStylesheetLink');
-const currentColor = localStorage.getItem("color") || "pink";
+function websiteColorChanging(themeColor) {
+	const colorSelect = document.querySelector('.color-circle');
+	const colorStylessheetLink = document.querySelector('.colorStylesheetLink');
+	const currentColor = localStorage.getItem("color") || "pink";
 
-function activateColor(themeColor) {
 	colorStylessheetLink.setAttribute("href", `css/colors/${themeColor}.css`);
 	localStorage.setItem("color", themeColor);
-	};
-
-
-//website theme color changing	
-const themeStylessheetLink = document.querySelector('.themeStylesheetLink');
-
-if (localStorage.getItem("theme") === "dark") {
-	themeStylessheetLink.setAttribute("href", `css/themes/dark.css`);
 }
 
-document.querySelector('.theme-button').addEventListener('click', function() {
-if (localStorage.getItem("theme") === "dark") 
-{
-	localStorage.setItem("theme", "light");
-	themeStylessheetLink.setAttribute("href", `css/themes/light.css`);
-} else {
-	localStorage.setItem("theme", "dark");
-	themeStylessheetLink.setAttribute("href", `css/themes/dark.css`);
+
+//website theme color changing
+function websiteThemeChanging() {
+	const themeStylessheetLink = document.querySelector('.themeStylesheetLink');
+
+	if (localStorage.getItem("theme") === "dark") {
+		themeStylessheetLink.setAttribute("href", `css/themes/dark.css`);
+	}
+
+	document.querySelector('.theme-button').addEventListener('click', function() {
+	if (localStorage.getItem("theme") === "dark") 
+	{
+		localStorage.setItem("theme", "light");
+		themeStylessheetLink.setAttribute("href", `css/themes/light.css`);
+	} else {
+		localStorage.setItem("theme", "dark");
+		themeStylessheetLink.setAttribute("href", `css/themes/dark.css`);
+	}
+	});
 }
-});
+websiteThemeChanging();
 
 
 //slides change
@@ -85,10 +96,51 @@ function changeSlide(n) {
 		slides[slideIndex].classList.add("right");
 	}
   }
-
 var slideIndex = 1;
 changeSlide(slideIndex);
 
 function plusIndex(n) {
 	changeSlide(slideIndex += n);
 }
+
+
+//copy email to clipboard
+function copyEmail() {
+	document.querySelector('#copy-button-email').addEventListener('click', function() {
+		navigator.permissions.query({name: "clipboard-write"}).then((result) => {
+			// If permission to write to the clipboard is granted or if the user will
+			// be prompted to allow it, we proceed.
+			if (result.state === 'granted' || result.state === 'prompt') {
+				navigator.clipboard.writeText("oliwiagowor@gmail.com")
+					.then(text => {
+						//message about successful copy
+					})
+					.catch(err => {
+						console.error("Failed to write to clipboard: ", err);
+					});
+			}	
+		});
+	});
+}
+copyEmail();
+
+
+//copy phone number to clipboard
+function copyPhone() {
+	document.querySelector('#copy-button-phone').addEventListener('click', function() {
+		navigator.permissions.query({name: "clipboard-write"}).then((result) => {
+			// If permission to write to the clipboard is granted or if the user will
+			// be prompted to allow it, we proceed.
+			if (result.state === 'granted' || result.state === 'prompt') {
+				navigator.clipboard.writeText("531252664")
+					.then(text => {
+						//message about successful copy 
+					})
+					.catch(err => {
+						console.error("Failed to write to clipboard: ", err);
+					});
+			}	
+		});
+	});
+}
+copyPhone();
